8d7002e428af531b5027499492309ce6a376118c
Aran Mulholland <aranmulholland@gmail.com>
2012-09-21 14:00:33 +1000

    Initial Commit

---
+++ loadKoTemplate.js
@@ -0,0 +1,13 @@
+﻿define("loadKoTemplate", ["text", "knockout"], function (text, ko) {
+   return {
+      load: function (name, req, load, config) {
+         var onLoad = function (content) {
+            ko.templates[name] = content;
+            load(content);
+         },
+         fullPath = config.loadKoTemplate.templatePath + name + config.loadKoTemplate.extension;
+         
+         text.load(fullPath, req, onLoad, config);
+      }
+   };
+});
