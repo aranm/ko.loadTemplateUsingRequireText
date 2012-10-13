131e0820e240ca5555d1fcf73eb2818f8e8658b8
Aran Mulholland <aranmulholland@gmail.com>
2012-10-13 21:15:16 +1100

    Added ability to optionally use a global config to specify base template path and file extension.

--- loadKoTemplate.js
+++ loadKoTemplate.js
@@ -1,31 +1,51 @@
-﻿﻿define(["text", "knockout", "stringTemplateEngine"], function (text, ko) {
-	var loader = {
-		load: function (name, req, load, config) {
-			var onLoad = function (content) {
-				var safeName = inferTemplateName(name);
-				ko.templates[safeName] = content;
-				load(content);
-			};
+﻿define(["text", "knockout", "stringTemplateEngine"], function (text, ko) {
+   var loader = {
+      load: function (name, req, load, config) {
+         var onLoad = function (content) {
+            var safeName = inferTemplateName(name);
+            ko.templates[safeName] = content;
+            load(content);
+         },
+         templateName = name,
+         index;
+         
+         //use the global configuration if it is defined
+         if (config !== undefined && config.loadKoTemplate !== undefined) {
+            if (config.loadKoTemplate.templatePath !== undefined) {
+               templateName = config.loadKoTemplate.templatePath + templateName;
+            }
+            if (config.loadKoTemplate.extension !== undefined) {
+               
+               //if we have a ! in the path we only want the text before it
+               index = templateName.indexOf("!");
+               if (index !== -1) {
+                  templateName = templateName.substring(0, index);
+               }
+               templateName = templateName + config.loadKoTemplate.extension;
+            }
+         }
 
-			text.load(name, req, onLoad, config);
-		}
-	};
+         text.load(templateName, req, onLoad, config);
+      }
+   };
 
-	function inferTemplateName(name) {
-		var templateName;
+   function inferTemplateName(name) {
+      var templateName,
+      index = name.indexOf("!"),
+      parts;
+      
+      if (index !== -1) {
+         //use the template name that is specified
+         templateName = name.substring(index + 1, name.length);
+      }
+      else {
+         //use the file name sans the path as the template name
+         parts = name.split("/");
+         templateName = parts[parts.length - 1].split(".").join("-");
+      }
 
-		var index = name.indexOf("!");
-		if (index !== -1) {
-			//use the template name that is specified
-			templateName = name.substring(index + 1, name.length);
-		} else {
-			//use the file name sans the path as the template name
-			var parts = name.split("/");
-			templateName = parts[parts.length - 1].split(".").join("-");
-		}
+      return templateName;
+   }
 
-		return templateName;
-	}
-
-	return loader;
+   return loader;
 });
